6c826d333041265f75cc07df83f4a231ce824cbb
mcaleg2006 <emil720apro@gmail.com>
2025-10-19 23:22:52 +0300

    +

---
+++ DevQuestions/src/DevQuestion.Infrastructure.Communication/UsersCommunicationService.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using DevQuestions.Application.Communication;
+using Shared;
+
+namespace DevQuestion.Infrastructure.Communication;
+
+public class UsersCommunicationService : IUsersCommunicationService
+{
+    public Task<Result<long, Failure>> GetUserRatingAsync(Guid userId, CancellationToken cancellationToken = default) => 
+        throw new NotImplementedException();
+}
