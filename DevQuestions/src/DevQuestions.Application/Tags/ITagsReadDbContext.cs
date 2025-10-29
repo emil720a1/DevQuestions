e79d18c2400f99d88c9d675e843bec326d9dc10c
mcaleg2006 <emil720apro@gmail.com>
2025-10-29 11:11:47 +0200

    фикс

--- DevQuestions/src/DevQuestions.Application/Tags/ITagsReadDbContext.cs
+++ DevQuestions/src/DevQuestions.Application/Tags/ITagsReadDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Amazon.S3.Model;
-
-
-namespace DevQuestions.Application.Tags;
-
-public interface ITagsReadDbContext
-{
-    IQueryable<Tag> TagsRead { get; }
-}
+﻿// using Amazon.S3.Model;
+//
+//
+// namespace DevQuestions.Application.Tags;
+//
+// public interface ITagsReadDbContext
+// {
+//     IQueryable<Tag> TagsRead { get; }
+// }
